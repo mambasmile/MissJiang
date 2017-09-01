571a50c252e86d7e4218867ea1db81aea61d903d
edsonw <969693577@qq.com>
2017-09-01 16:36:49 +0800

    update

---
+++ feature_mining/load_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/1 15:33
+# @Author  : sunday
+# @Site    : 
+# @File    : load_data.py
+# @Software: PyCharm
+
+def load_data():
+    train_ud_mdl = pd.read_csv('../data/lexin_train02/ud_mdl.csv', index_col=0)
+    #未来六个月的消费信息及dep
+    train_dep_mdl = pd.read_csv('../data/lexin_train02/dep_mdl.csv', index_col=0)
+    #过去六个月用户场景行为信息
+    train_login_scene_mdl = pd.read_csv('../data/lexin_train02/login_scene_mdl.csv', index_col=0)
+    #过去六个月新增订单明细数据
+    train_od_in6m_mdl = pd.read_csv('../data/lexin_train02/od_in6m_mdl.csv', index_col=0)
+    #过去六个月订单行为汇总
+    train_p6M_mdl = pd.read_csv('../data/lexin_train02/p6M_mdl.csv', index_col=0)
+    #过去12个月月度订单金额
+    train_p12M_mdl = pd.read_csv('../data/lexin_train02/p12M_mdl.csv', index_col=0)
--- feature_mining/pre_process_data.py
+++ feature_mining/pre_process_data.py
@@ -8,7 +8,8 @@
 
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sbn
 
 def pre_process():
     train_ud_mdl = pd.read_csv('../data/lexin_train02/ud_mdl.csv',index_col=0)
@@ -19,13 +20,16 @@ def pre_process():
     # train_login_scene_mdl = pd.read_csv('../data/lexin_train02/login_scene_mdl.csv', index_col=0)
     # #过去六个月新增订单明细数据
     # train_od_in6m_mdl = pd.read_csv('../data/lexin_train02/od_in6m_mdl.csv', index_col=0)
-    # #过去六个月订单行为汇总
-    # train_p6M_mdl = pd.read_csv('../data/lexin_train02/p6M_mdl.csv', index_col=0)
+    #过去六个月订单行为汇总
+    train_p6M_mdl = pd.read_csv('../data/lexin_train02/p6M_mdl.csv', index_col=0)
     # #过去12个月月度订单金额
     # train_p12M_mdl = pd.read_csv('../data/lexin_train02/p12M_mdl.csv', index_col=0)
 
     train_ud_mdl["fauth_source_type"] = train_ud_mdl['fauth_source_type'].astype(str)
     train_ud_mdl["fsex"] = train_ud_mdl['fsex'].astype(str)
+    #删除没用的特征
+    train_ud_mdl.pop('fregister_time')
+    train_ud_mdl.pop('fpocket_auth_time')
     train_ud_mdl.pop('fdomicile_provice')
     train_ud_mdl.pop('fdomicile_city')
     train_ud_mdl.pop('fdomicile_area')
@@ -33,12 +37,32 @@ def pre_process():
     train_ud_mdl.pop('sch_fcity_name')
     train_ud_mdl.pop('sch_fregion_name')
     train_ud_mdl.pop('sch_fcompany_name')
+    train_ud_mdl.pop('fschoolarea_name_md5')
 
-    print  pd.get_dummies(train_ud_mdl["fauth_source_type"],prefix="fauth_source_type").head()
-    print train_ud_mdl['fschoolarea_name_md5'].value_counts()
+    plt.show(train_ud_mdl['fstd_num'].hist())
+    # print  pd.get_dummies(train_ud_mdl["fauth_source_type"],prefix="fauth_source_type").head()
 
-    # pd.get_dummies(train_ud_mdl)
+    #进行one-hot编码
+    all_ud_dummy_df = pd.get_dummies(train_ud_mdl,columns=['fsex','fis_entrance_exam','fcollege_level','fauth_source_type','fcal_graduation'])
 
-    print  train_ud_mdl.columns
+
+
+
+
+
+
+
+
+
+
+    print all_ud_dummy_df.columns
+    #检查数据缺失情况
+    print all_dummy_df.isnull().sum().sort_values(ascending=False).head(15)
+    print  all_dummy_df.isnull().sum().sum()
+    numeric_cols = all_dummy_df.columns[all_dummy_df.dtypes != 'object']
+    print numeric_cols
+    #按照第一列纵向合并数据
+    # res = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
+    # print  train_ud_mdl.columns
 
 pre_process()
