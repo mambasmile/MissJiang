331883b8c48f4dd0dd9c1cd5c1e8ed6f45a5f871
edsonw <969693577@qq.com>
2017-09-01 09:28:16 +0800

    pre process data

---
+++ feature_mining/pre_process_data.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/31 20:55
+# @Author  : sunday
+# @Site    : 
+# @File    : pre_process_data.py
+# @Software: PyCharm
+
+import numpy as np
+import pandas as pd
+
+
+def pre_process():
+    train_ud_mdl = pd.read_csv('../data/lexin_train02/ud_mdl.csv',index_col=0)
+
+    # #未来六个月的消费信息及dep
+    # train_dep_mdl = pd.read_csv('../data/lexin_train02/dep_mdl.csv', index_col=0)
+    # #过去六个月用户场景行为信息
+    # train_login_scene_mdl = pd.read_csv('../data/lexin_train02/login_scene_mdl.csv', index_col=0)
+    # #过去六个月新增订单明细数据
+    # train_od_in6m_mdl = pd.read_csv('../data/lexin_train02/od_in6m_mdl.csv', index_col=0)
+    # #过去六个月订单行为汇总
+    # train_p6M_mdl = pd.read_csv('../data/lexin_train02/p6M_mdl.csv', index_col=0)
+    # #过去12个月月度订单金额
+    # train_p12M_mdl = pd.read_csv('../data/lexin_train02/p12M_mdl.csv', index_col=0)
+
+    train_ud_mdl["fauth_source_type"] = train_ud_mdl['fauth_source_type'].astype(str)
+    train_ud_mdl["fsex"] = train_ud_mdl['fsex'].astype(str)
+    train_ud_mdl.pop('fdomicile_provice')
+    train_ud_mdl.pop('fdomicile_city')
+    train_ud_mdl.pop('fdomicile_area')
+    train_ud_mdl.pop('sch_fprovince_name')
+    train_ud_mdl.pop('sch_fcity_name')
+    train_ud_mdl.pop('sch_fregion_name')
+    train_ud_mdl.pop('sch_fcompany_name')
+
+    print  pd.get_dummies(train_ud_mdl["fauth_source_type"],prefix="fauth_source_type").head()
+    print train_ud_mdl['fschoolarea_name_md5'].value_counts()
+
+    # pd.get_dummies(train_ud_mdl)
+
+    print  train_ud_mdl.columns
+
+pre_process()
