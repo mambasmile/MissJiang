0910ce030b5f76dcadce60ab2ae690aaa52e6bcc
mambasmile <mambasmile@users.noreply.github.com>
2018-02-27 15:22:08 +0800

    Delete __init__.py

--- feature_mining/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/8/31 20:53
-# @Author  : sunday
-# @Site    : 
-# @File    : __init__.py.py
-# @Software: PyCharm
-
-from .feature_mining import *
